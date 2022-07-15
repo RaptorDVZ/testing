f13522d4b6ddbb1639f43fb5ad0261fdbf733282
Divyanshu <sdivyanshu105@gmail.com>
2022-07-15 10:29:17 +0530

    second

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port= process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.send("<h1>Hellos!!!</h1>")
+    res.send("<h1>Hello!</h1>")
 })
 
 app.listen(3000,()=>{
